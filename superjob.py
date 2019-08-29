42a98ff7509e88d9013c5e17598b38cdd813c4e8
Brizzardis <37468993+Brizzardis@users.noreply.github.com>
2019-08-29 16:34:36 +0500

    Update superjob.py

--- superjob.py
+++ superjob.py
@@ -1,1 +1,22 @@
 from bs4 import BeautifulSoup as bs
+import requests
+
+
+html = requests.get('https://www.superjob.ru/vakansii/programmist.html').text
+
+parsed_html = bs(html,'html.parser')
+
+data_list = parsed_html.findAll("div", {"class": "_3zucV _2GPIV i6-sc _3VcZr"})
+
+list_job = []
+
+for i in data_list:
+    name = i.find('div', {'class': '_3mfro'}).text
+    salary = i.find('span', {'class': 'f-test-text-company-item-salary'}).text
+    link = i.find('a', {'class': 'icMQ_'})
+    link = 'https://www.superjob.ru' + link['href']
+
+    list_job.append(name + ' ' + salary + ' ' + link)
+
+for i in list_job:
+    print(i)
